fbfd6573ede4cd5f86754628ea5af07d3a0f5805
LENOVO <LENOVO@LAPTOP-7AUJMFLH>
2023-03-12 18:29:46 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { set; get; }
+        public string Date { set; get; }
+        public string Time { set; get; }
+        public byte Category { set; get; }
+        public IEnumerable<Category> Categories { set; get; }
+        public DateTime GetDateTime()
+        { 
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time)); 
+        }
+    }
+}
